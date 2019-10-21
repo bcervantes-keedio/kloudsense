dd528b2a37363366f0d599de7e4617def1f78752
r2dedios <alex.ansi.c@gmail.com>
2019-10-21 15:12:23 +0000

    Script to install ssh public keys based on inventory file

---
+++ tools/ssh_key_installer.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+KS_HOME="."
+source config/kloudsense.properties
+
+
+INV_FILE=$KS_DCOS_INVENTORY_FILE
+
+
+hosts=$(grep -v -e "^$" -e "^\[.*$" -e ".*ansible_connection.*" $INV_FILE | sort | uniq)
+
+for host in ${hosts[@]}; do
+  echo "Installing SSH-KEY in: $host"
+  ssh-copy-id -i ~/.ssh/id_rsa.pub centos@$host
+  ssh-keyscan -H $host >> ~/.ssh/known_hosts
+done
