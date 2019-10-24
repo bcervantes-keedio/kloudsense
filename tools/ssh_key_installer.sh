706d61c4c2615933f0b3d7a60518b0fe89c6b166
Alejandro Villegas <alex.ansi.c@gmail.com>
2019-10-24 16:12:17 +0000

    Added support for type username as cmd-arg

--- tools/ssh_key_installer.sh
+++ tools/ssh_key_installer.sh
@@ -10,7 +10,7 @@ INV_FILE=$KS_DCOS_INVENTORY_FILE
 hosts=$(grep -v -e "^$" -e "^\[.*$" -e ".*ansible_connection.*" $INV_FILE | sort | uniq)
 
 for host in ${hosts[@]}; do
-  echo "Installing SSH-KEY in: $host"
-  ssh-copy-id -i ~/.ssh/id_rsa.pub centos@$host
+  echo "Installing SSH-KEY in: $1@$host"
+  ssh-copy-id -i ~/.ssh/id_rsa.pub $1@$host
   ssh-keyscan -H $host >> ~/.ssh/known_hosts
 done
